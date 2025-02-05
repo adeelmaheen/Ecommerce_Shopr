0227ee8b218fd8ecf944fde90c0403d0e926fbbf
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2025-02-05 09:34:46 +0500

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -158,9 +158,9 @@ const Header = () => {
             {user && (
               <button
                 onClick={createClerkPasskey}
-                className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+                className=" bg-white  hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
               >
-                Create Passkey
+                Passkey
               </button>
             )}
           </ClerkLoaded>
